913cc3e364cd642217673de7a3094f052153f20c
dx <darcyzhangdx@gmail.com>
2025-12-21 23:27:14 +0800

    added forgoten comma

--- keyboards/planck/rev7/keymaps/dxddxx/keymap.c
+++ keyboards/planck/rev7/keymaps/dxddxx/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x12
         (
    LT(1,KC_Q),   KC_W,    KC_E,         KC_R,    KC_T,    KC_Y,         KC_U,    KC_I,    KC_O,      LT(2,KC_P),    KC_MINS, KC_EQL,
-        KC_A,    KC_S,    KC_D,         KC_F     KC_G,    KC_H,         KC_J,    KC_K,    KC_L,      LT(4,KC_SCLN),  KC_HOME, KC_PGUP,
+        KC_A,    KC_S,    KC_D,         KC_F,    KC_G,    KC_H,         KC_J,    KC_K,    KC_L,      LT(4,KC_SCLN), KC_HOME, KC_PGUP,
  LSFT_T(KC_Z),   KC_X,    KC_C,    LT(1,KC_V),   KC_B,    KC_N,    LT(2,KC_M),   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_END,  KC_PGDN,
         KC_LCTL, KC_LALT, KC_LGUI,      KC_SPC,  MS_BTN1, MS_BTN2,      KC_SPC,  KC_RGUI, KC_RALT,        KC_RCTL,  KC_VOLD, KC_VOLU
         ),
