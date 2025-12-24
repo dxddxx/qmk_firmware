aeaa712009680687cb209f3121a6f76cffc730aa
dx <darcyzhangdx@gmail.com>
2025-12-24 23:24:04 +0800

    rhs is now numbers

--- keyboards/planck/rev7/keymaps/dxddxx/keymap.c
+++ keyboards/planck/rev7/keymaps/dxddxx/keymap.c
@@ -34,45 +34,45 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x12
         (
-   LT(1,KC_Q),   KC_W,    KC_E,         KC_R,    KC_T,    KC_Y,         KC_U,    KC_I,    KC_O,      LT(2,KC_P),    KC_MINS, KC_EQL,
-        KC_A,    KC_S,    KC_D,         KC_F,    KC_G,    KC_H,         KC_J,    KC_K,    KC_L,      LT(4,KC_SCLN), KC_HOME, KC_PGUP,
- LSFT_T(KC_Z),   KC_X,    KC_C,    LT(1,KC_V),   KC_B,    KC_N,    LT(2,KC_M),   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_END,  KC_PGDN,
-        KC_LCTL, KC_LALT, KC_LGUI,      KC_SPC,  MS_BTN1, MS_BTN2,      KC_SPC,  KC_RGUI, KC_RALT,        KC_RCTL,  KC_VOLD, KC_VOLU
+   LT(1,KC_Q),   KC_W,    KC_E,         KC_R,    KC_T,    KC_Y,         KC_U,    KC_I,    KC_O,      LT(2,KC_P),    KC_P6,   KC_P7,
+        KC_A,    KC_S,    KC_D,         KC_F,    KC_G,    KC_H,         KC_J,    KC_K,    KC_L,      LT(4,KC_SCLN), KC_P4,   KC_P5,
+ LSFT_T(KC_Z),   KC_X,    KC_C,    LT(1,KC_V),   KC_B,    KC_N,    LT(2,KC_M),   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_P2,   KC_P3,
+        KC_LCTL, KC_LALT, KC_LGUI,      KC_SPC,  MS_BTN1, MS_BTN2,      KC_SPC,  KC_RGUI, KC_RALT,        KC_RCTL,  KC_P0,   KC_P1
         ),
     [1] = LAYOUT_ortho_4x12
         (
         KC_1,    KC_2,    KC_3,         KC_4,    KC_5,    KC_CIRC,      KC_AMPR, KC_ASTR, KC_LPRN,        KC_RPRN,  XXXXXXX, XXXXXXX,
         KC_GRV,  XXXXXXX, MO(5),        KC_VOLU, MS_WHLU, KC_HOME,      KC_UNDS, KC_PLUS, KC_END,         KC_DQUO,  XXXXXXX, XXXXXXX,
         KC_LSFT, KC_BSPC, KC_CAPS,      KC_VOLD, MS_WHLD, MS_WHLR,      XXXXXXX, KC_LCBR, KC_RCBR,        KC_PIPE,  XXXXXXX, XXXXXXX,
-        _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  _______, _______
+        _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  XXXXXXX, XXXXXXX
         ),
     [2] = LAYOUT_ortho_4x12
         (
         KC_EXLM, KC_AT,   KC_HASH,      KC_DLR,  KC_PERC, KC_6,         KC_7,    KC_8,    KC_9,           KC_0,     XXXXXXX, XXXXXXX,
         KC_TILD, XXXXXXX, XXXXXXX,      KC_BRMU, MS_WHLU, KC_PGUP,      KC_MINS, KC_EQL,  KC_PGDN,        KC_QUOT,  XXXXXXX, XXXXXXX,
         KC_LSFT, KC_DEL,  KC_TAB,       KC_BRMD, MS_WHLL, MS_WHLR,      TG(3),   KC_LBRC, KC_RBRC, RSFT_T(KC_BSLS), XXXXXXX, XXXXXXX,
-        _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  _______, _______
+        _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  XXXXXXX, XXXXXXX
         ),
     [3] = LAYOUT_ortho_4x12
         (
         KC_1,    KC_2,    KC_3,         KC_4,    KC_5,    KC_6,         KC_7,    KC_8,    KC_9,           KC_0,     XXXXXXX, XXXXXXX,
         KC_GRV,  XXXXXXX, XXXXXXX,      KC_VOLU, MS_WHLU, KC_PGUP,      KC_MINS, KC_EQL,  KC_PGDN,        KC_QUOT,  XXXXXXX, XXXXXXX,
         KC_LSFT, KC_BSPC, KC_CAPS,      KC_VOLD, MS_WHLD, MS_WHLR,      TG(3),   KC_LBRC, KC_RBRC, RSFT_T(KC_BSLS), XXXXXXX, XXXXXXX,
-        _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  _______, _______
+        _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  XXXXXXX, XXXXXXX
         ),
     [4] = LAYOUT_ortho_4x12
         (
         KC_F1,   KC_F2,   KC_F3,        KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,          KC_F10,   XXXXXXX, XXXXXXX,
         KC_ESC,  XXXXXXX, XXXXXXX,      XXXXXXX, UG_TOGG, KC_LEFT,      KC_DOWN, KC_UP,   KC_RGHT,        XXXXXXX,  XXXXXXX, XXXXXXX,
         KC_LSFT, KC_INS,  KC_ENT,       XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, KC_F11,  KC_F12,         KC_RSFT,  XXXXXXX, XXXXXXX,
-        _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  _______, _______
+        _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  XXXXXXX, XXXXXXX
         ),
     [5] = LAYOUT_ortho_4x12
         (
         XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, KC_NUM,       KC_P7,   KC_P8,    KC_P9,         KC_PENT,  XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, KC_PPLS,      KC_P4,   KC_P5,    KC_P6,         KC_PAST,  XXXXXXX, XXXXXXX,
         KC_LSFT, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, KC_PMNS,      KC_P1,   KC_P2,    KC_P3,         KC_PSLS,  XXXXXXX, XXXXXXX,
-        _______, _______, _______,      _______, _______, _______,      KC_P0,   KC_PDOT, _______,        _______,  _______, _______
+        _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  XXXXXXX, XXXXXXX
         )
 };
 
