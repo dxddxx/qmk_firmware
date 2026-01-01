26386ce78c5ef1dcacdae9e79a624de4c083bf0d
dx <darcyzhangdx@gmail.com>
2026-01-02 01:12:45 +0800

    v and m now go to layer 3

--- keyboards/planck/rev7/keymaps/dxddxx/keymap.c
+++ keyboards/planck/rev7/keymaps/dxddxx/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         (
    LT(1,KC_Q),   KC_W,    KC_E,         KC_R,    KC_T,    KC_Y,         KC_U,    KC_I,    KC_O,      LT(2,KC_P),    KC_P8,   KC_P9,
         KC_A,    KC_S,    KC_D,         KC_F,    KC_G,    KC_H,         KC_J,    KC_K,    KC_L,      LT(4,KC_SCLN), KC_P6,   KC_P7,
- LSFT_T(KC_Z),   KC_X,    KC_C,    LT(1,KC_V),   KC_B,    KC_N,    LT(2,KC_M),   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_P4,   KC_P5,
+ LSFT_T(KC_Z),   KC_X,    KC_C,    LT(3,KC_V),   KC_B,    KC_N,    LT(3,KC_M),   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_P4,   KC_P5,
         KC_LCTL, KC_LALT, KC_LGUI,      KC_SPC,  MS_BTN1, MS_BTN2,      KC_SPC,  KC_RGUI, KC_RALT,        KC_P1,    KC_P2,   KC_P3
         ),
     [1] = LAYOUT_ortho_4x12
@@ -52,14 +52,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         (
         KC_EXLM, KC_AT,   KC_HASH,      KC_DLR,  KC_PERC, KC_6,         KC_7,    KC_8,    KC_9,           KC_0,     _______, _______,
         KC_TILD, XXXXXXX, XXXXXXX,      KC_BRMU, MS_WHLU, KC_PGUP,      KC_MINS, KC_EQL,  KC_PGDN,        KC_QUOT,  _______, _______,
-        KC_LSFT, KC_DEL,  KC_TAB,       KC_BRMD, MS_WHLL, MS_WHLR,      TG(3),   KC_LBRC, KC_RBRC, RSFT_T(KC_BSLS), _______, _______,
+        KC_LSFT, KC_DEL,  KC_TAB,       KC_BRMD, MS_WHLL, MS_WHLR,      XXXXXXX, KC_LBRC, KC_RBRC, RSFT_T(KC_BSLS), _______, _______,
         _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  _______, _______
         ),
     [3] = LAYOUT_ortho_4x12
         (
         KC_1,    KC_2,    KC_3,         KC_4,    KC_5,    KC_6,         KC_7,    KC_8,    KC_9,           KC_0,     _______, _______,
         KC_GRV,  XXXXXXX, XXXXXXX,      KC_VOLU, MS_WHLU, KC_PGUP,      KC_MINS, KC_EQL,  KC_PGDN,        KC_QUOT,  _______, _______,
-        KC_LSFT, KC_BSPC, KC_CAPS,      KC_VOLD, MS_WHLD, MS_WHLR,      TG(3),   KC_LBRC, KC_RBRC, RSFT_T(KC_BSLS), _______, _______,
+        KC_LSFT, KC_BSPC, KC_CAPS,      KC_VOLD, MS_WHLD, MS_WHLR,      XXXXXXX, KC_LBRC, KC_RBRC, RSFT_T(KC_BSLS), _______, _______,
         _______, _______, _______,      _______, _______, _______,      _______, _______, _______,        _______,  _______, _______
         ),
     [4] = LAYOUT_ortho_4x12
